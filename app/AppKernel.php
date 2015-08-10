e24e133c0894d089408e27ec8ec9c5b626813d3d
MaksimPW <ma.savichev@gmail.com>
2015-08-10 19:56:34 +0600

    pagination

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new M4\MinecraftBundle\M4MinecraftBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/M4/MinecraftBundle/Controller/DefaultController.php
+++ src/M4/MinecraftBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace M4\MinecraftBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use M4\MinecraftBundle\Entity\Product;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
@@ -24,21 +25,36 @@ class DefaultController extends Controller
         return new Response('Created product id '.$product->getId());
     }
 
-    public function showAction()
-    {
-        $name= 'name'; 
-        return $this->render('M4MinecraftBundle:Default:index.html.twig', array('name' => $name));
-    }
-
     public function indexAction()
     {
-        $mc_server = $this->getDoctrine()
+        $em = $this->getDoctrine()->getEntityManager();
+        $dql="SELECT m FROM M4MinecraftBundle:Mc_server m ORDER BY m.balls DESC";
+        $query = $em->createQuery($dql);
+        //$mc_server = $query->getResult();
+
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $query,
+            $this->get('request')->query->get('page', 1)/*page number*/,
+            2 /*limit per page */
+        );
+
+        return $this->render('M4MinecraftBundle:Default:index.html.twig', array('pagination' => $pagination));
+    }
+
+    public function showAction()
+    {
+        /* $mc_server = $this->getDoctrine()
             ->getRepository('M4\MinecraftBundle\Entity\Mc_server')
             ->findAll();
+*/
+        $em = $this->getDoctrine()->getEntityManager();
+        $dql="SELECT m FROM M4MinecraftBundle:Mc_server m ORDER BY m.balls DESC";
+        $query = $em->createQuery($dql);
+        $mc_server = $query->getResult();
 
 
-
-        return $this->render('M4MinecraftBundle:Default:index.html.twig', array('mc_server' => $mc_server));
+        return $this->render('::base.html.twig', array('mc_server' => $mc_server));
         //return new Response('Show Name: '.$product->getName());
     }
 
