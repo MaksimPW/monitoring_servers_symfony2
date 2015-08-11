b39fdf2e5e2e2154546984635101f01a0b6442b4
MaksimPW <ma.savichev@gmail.com>
2015-08-11 16:14:53 +0600

    correction view

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new M4\MinecraftBundle\M4MinecraftBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
