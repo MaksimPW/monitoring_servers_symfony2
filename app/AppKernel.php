2aa6fe4baa6cb523f6fc95890b31771ebb2d4edb
MaksimPW <ma.savichev@gmail.com>
2015-09-13 15:55:16 +0600

    add RobokassaBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new M4\TaskBundle\M4TaskBundle(),
+            new M4\RobokassaBundle\M4RobokassaBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/M4/RobokassaBundle/M4RobokassaBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace M4\RobokassaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class M4RobokassaBundle extends Bundle
+{
+}
